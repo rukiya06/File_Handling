5919a28af476e06ca45f2d255937007bd6440864
ANSHU_RUKIYA <skrukiya06@gmail.com>
2025-07-12 11:50:34 +0530

    Add files via upload

---
+++ WriteMethod/W_method.py
@@ -0,0 +1,5 @@
+file=open("file.txt","w")
+data=file.write("""This is about writing contents into the file even without the existing of file
+                By the W method, it can create the file with that name and write the contents """)
+#print(data)
+file.close()
