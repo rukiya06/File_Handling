6f7d5560b2355411b618a75370ec3bca9f4e1608
ANSHU_RUKIYA <skrukiya06@gmail.com>
2025-07-17 15:42:04 +0530

    Delete SerialNumbered(data deletion)/line.py

--- SerialNumbered(data deletion)/line.py
+++
@@ -1,9 +0,0 @@
-#serial numbered data-deletion
-
-ldel=input("enter exact line:")
-with open('log.txt','r')as f:
-    lines=f.readlines()
-with open('log.txt','w') as f:
-    for line in lines:
-        if line.strip()!=ldel.strip():
-            f.write(line)
